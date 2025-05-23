dab9ae2bf1835f4aad98a921cce366dec56391f8
samezzz <psalmuelselfie@gmai.com>
2025-05-23 07:09:11 +0000

    batman

--- lua/kickstart/plugins/snacks.lua
+++ lua/kickstart/plugins/snacks.lua
@@ -20,7 +20,47 @@ return {
         scope = { enabled = true }, -- Jumps: ]i [i textobjects: ii(inner scope) ai(full scope)
         input = { enabled = true },
         image = { enabled = true, force = true },
-        dashboard = { enabled = true },
+        dashboard = {
+            enabled = true,
+            preset = {
+                header = [[
+          ███████╗ █████╗ ███╗   ███╗███████╗███████╗███████╗
+          ██╔════╝██╔══██╗████╗ ████║██╔════╝██╔════╝██╔════╝
+          ███████╗███████║██╔████╔██║█████╗  ███████╗███████╗
+          ╚════██║██╔══██║██║╚██╔╝██║██╔══╝  ╚════██║╚════██║
+          ███████║██║  ██║██║ ╚═╝ ██║███████╗███████║███████║
+          ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝
+        ]],
+            },
+            sections = {
+                { section = "header" },
+                {
+                    pane = 2,
+                    section = "terminal",
+                    cmd = "colorscript -e square",
+                    height = 5,
+                    padding = 1,
+                },
+                { section = "keys", gap = 1, padding = 1 },
+                { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+                { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+                {
+                    pane = 2,
+                    icon = " ",
+                    title = "Git Status",
+                    section = "terminal",
+                    enabled = function()
+                        return Snacks.git.get_root() ~= nil
+                    end,
+                    cmd = "git status --short --branch --renames",
+                    height = 5,
+                    padding = 1,
+                    ttl = 5 * 60,
+                    indent = 3,
+                },
+                { section = "startup" },
+            },
+        },
         picker = {
             matcher = { frecency = true },
             win = {
